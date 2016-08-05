35b8f3d4e227b88c7b80b5e4d01274936dea47c0
narutoswj <narutoswj@hotmail.com>
2016-08-05 17:18:11 +0800

    initial commit

---
+++ ExcelAddIn1/Ribbon1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace ExcelAddIn1
+{
+    public partial class Crabyter
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Globals.ThisAddIn.Application.ActiveCell.NumberFormatLocal = "@";
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            int number = Globals.ThisAddIn.Application.ActiveCell.Column;
+            Globals.ThisAddIn.Application.Cells[1][1] = number;
+        }
+    }
+}
--- ExcelAddIn1/ThisAddIn.cs
+++ ExcelAddIn1/ThisAddIn.cs
@@ -13,12 +13,23 @@ namespace ExcelAddIn1
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            this.Application.WorkbookBeforeSave += new Microsoft.Office.Interop.Excel.AppEvents_WorkbookBeforeSaveEventHandler(Application_WorkbookBeforeSave);
+           
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
         }
+        void Application_WorkbookBeforeSave(Microsoft.Office.Interop.Excel.Workbook Wb, bool SaveAsUI, ref bool Cancel)
+        {
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Application.ActiveSheet);
+            Excel.Range firstRow = activeWorksheet.get_Range("A1");
+            firstRow.EntireRow.Insert(Excel.XlInsertShiftDirection.xlShiftDown);
+            Excel.Range newFirstRow = activeWorksheet.get_Range("A1");
+            newFirstRow.Value2 = "This text was added by using code";
+        }
 
+        
         #region VSTO 生成的代码
 
         /// <summary>
